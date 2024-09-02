e1b5085f60b7a29007d047d75b5143e804433d80
juanluiscontreras <45545139+juanluiscontreras@users.noreply.github.com>
2024-09-01 21:40:57 -0300

    more fixes on details

--- app/components/CarDetails.tsx
+++ app/components/CarDetails.tsx
@@ -4,371 +4,371 @@ import React from 'react';
 const CarDetails: React.FC = () => {
   return (
     <div className="container section">
-     <h1>Especificaciones del Suzuki Swift 1.5 VVT 2007</h1>
+      <h1>Especificaciones del Suzuki Swift 1.5 VVT 2007</h1>
 
-<details>
-    <summary>Prestaciones y consumos homologados</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Prestaciones y consumos homologados</summary>
+        <table>
+          <tr>
             <th>Velocidad máxima</th>
             <td>185 km/h</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Aceleración 0-100 km/h</th>
             <td>10 s</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Consumo NEDC</th>
             <td></td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>Extraurbano</td>
             <td>5,3 l/100 km</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>Urbano</td>
             <td>8,6 l/100 km</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>Medio</td>
             <td>6,5 l/100 km</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Emisiones de CO₂ NEDC</th>
             <td>159 gr/km</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Distintivo ambiental DGT</th>
             <td>C</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Dimensiones, peso, capacidades</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Dimensiones, peso, capacidades</summary>
+        <table>
+          <tr>
             <th>Tipo de Carrocería</th>
             <td>Turismo</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Número de puertas</th>
             <td>5</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Longitud</th>
             <td>3.695 mm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Anchura</th>
             <td>1.690 mm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Altura</th>
             <td>1.500 mm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Batalla</th>
             <td>2.380 mm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Vía delantera</th>
             <td>1.470 mm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Vía trasera</th>
             <td>1.480 mm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Peso</th>
             <td>1.000 kg</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Depósito de combustible - Gasolina</th>
             <td>45 l</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Filtro de partículas</th>
             <td>No</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Volúmenes de maletero</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Volúmenes de maletero</summary>
+        <table>
+          <tr>
             <th>Volumen con una fila de asientos disponible</th>
             <td>562 litros</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Volumen mínimo con dos filas de asientos disponibles</th>
             <td>213 litros</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Número de plazas</th>
             <td>5</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Distribución de asientos</th>
             <td>2 + 3</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Resumen del sistema de propulsión</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Resumen del sistema de propulsión</summary>
+        <table>
+          <tr>
             <th>Potencia máxima</th>
             <td>102 CV / 75 kW</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Par máximo</th>
             <td>133 Nm</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Motor de Combustión</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Motor de Combustión</summary>
+        <table>
+          <tr>
             <th>Propósito</th>
             <td>Impulsar el vehículo</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Combustible</th>
             <td>Gasolina</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Potencia máxima</th>
             <td>102 CV / 75 kW</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Revoluciones potencia máxima</th>
             <td>5.900 rpm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Par máximo</th>
             <td>133 Nm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Revoluciones par máximo</th>
             <td>4.100 rpm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Situación</th>
             <td>Delantero transversal</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Número de cilindros</th>
             <td>4</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Disposición de los cilindros</th>
             <td>En línea</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Material del bloque</th>
             <td>Aluminio</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Material de la culata</th>
             <td>Aluminio</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Diámetro</th>
             <td>78 mm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Carrera</th>
             <td>78 mm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Cilindrada</th>
             <td>1.491 cm³</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Relación de compresión</th>
             <td>9,5 a 1</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Distribución</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Distribución</summary>
+        <table>
+          <tr>
             <th>Válvulas por cilindro</th>
             <td>4</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Tipo de distribución</th>
             <td>Dos árboles de levas en la culata</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Alimentación</th>
             <td>Inyección Indirecta</td>
-        </tr>
-        <tr>
-            <th>Automatismo de parada y arranque del motor ("Stop/Start")</th>
+          </tr>
+          <tr>
+            <th>Automatismo de parada y arranque del motor (Stop/Start)</th>
             <td>No</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Transmisión</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Transmisión</summary>
+        <table>
+          <tr>
             <th>Tracción</th>
             <td>Delantera</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Caja de cambios</th>
             <td>Manual</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Número de velocidades</th>
             <td>5</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Desarrollos (km/h cada 1.000 rpm)</th>
             <td></td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>1ª</td>
             <td>7,6</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>2ª</td>
             <td>14,1</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>3ª</td>
             <td>20,5</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>4ª</td>
             <td>27,7</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>5ª</td>
             <td>35,0</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>Marcha atrás</td>
             <td>8,3</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Chasis</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Chasis</summary>
+        <table>
+          <tr>
             <th>Estructura suspensión delantera</th>
             <td>McPherson</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Estructura suspensión trasera</th>
             <td>De barra de torsión</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Tipo de muelles delanteros</th>
             <td>Hélice</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Tipo de muelles traseros</th>
             <td>Hélice</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Barra estabilizadora delantera</th>
             <td>Sí</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Barra estabilizadora trasera</th>
             <td>Sí</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Frenos</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Frenos</summary>
+        <table>
+          <tr>
             <th>Freno delantero</th>
             <td>Disco ventilado</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Freno trasero</th>
             <td>Disco</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Diámetro freno delantero</th>
             <td>250 mm</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Diámetro freno trasero</th>
             <td>250 mm</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Ruedas y neumáticos</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Ruedas y neumáticos</summary>
+        <table>
+          <tr>
             <th>Neumáticos delanteros</th>
             <td>185/60 R15</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Neumáticos traseros</th>
             <td>185/60 R15</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Dirección</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Dirección</summary>
+        <table>
+          <tr>
             <th>Tipo de dirección</th>
             <td>Cremallera</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Tipo de asistencia</th>
             <td>Asistencia eléctrica</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Asistencia en función de la velocidad</th>
             <td>No</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Diámetro de giro entre bordillos</th>
             <td>9,4 m</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Vueltas de volante entre topes</th>
             <td>2,8</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
 
-<details>
-    <summary>Prestaciones y consumos WLTP</summary>
-    <table>
-        <tr>
+      <details>
+        <summary>Prestaciones y consumos WLTP</summary>
+        <table>
+          <tr>
             <th>Emisiones de CO₂ WLTP</th>
             <td>-</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th>Consumo WLTP</th>
             <td>-</td>
-        </tr>
-    </table>
-</details>
+          </tr>
+        </table>
+      </details>
     </div>
   );
 };
