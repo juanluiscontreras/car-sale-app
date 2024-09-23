5d67c905c6d26221b17443320f931982d79a64a4
juanluiscontreras <jlcontri@gmail.com>
2024-09-23 14:45:48 -0300

    fix: more photos and icons

--- app/components/CarFeatures.tsx
+++ app/components/CarFeatures.tsx
@@ -1,6 +1,6 @@
 'use client'; // Add this line at the top
 import { useTranslation } from 'react-i18next';
-import { FaCarSide, FaCogs, FaShieldAlt, FaLightbulb, FaWifi, FaLock, FaWrench, FaCheck, FaBluetooth, FaLuggageCart, FaSuitcase, FaSuitcaseRolling } from 'react-icons/fa';
+import { FaCarSide, FaCogs, FaShieldAlt, FaLightbulb, FaWifi, FaLock, FaWrench, FaCheck, FaBluetooth, FaLuggageCart, FaSuitcase, FaSuitcaseRolling, FaGasPump, FaMoneyBill } from 'react-icons/fa';
 
 const CarFeatures = () => {
   const { t } = useTranslation();
@@ -13,28 +13,28 @@ const CarFeatures = () => {
     { icon: <FaSuitcaseRolling />, text: t('features.foldableSeats') },
     { icon: <FaBluetooth />, text: t('features.bluetoothRadio') },
     { icon: <FaWrench />, text: t('features.chainEngine') },
-    { icon: <FaCheck />, text: t('features.point1') },
-    { icon: <FaCheck />, text: t('features.point2') },
-    { icon: <FaCheck />, text: t('features.point3') }
+    { icon: <FaGasPump />, text: t('features.point1') },
+    { icon: <FaMoneyBill />, text: t('features.point2') },
+    { icon: <FaCheck />, text: t('features.point3') },
   ];
 
   return (
-      <div className="max-w-6xl mx-auto px-4">
-        <h1 className="text-3xl md:text-4xl font-bold mb-6 text-center text-gray-800">
-          {t('features.title')}
-        </h1>
-        <div className="grid grid-cols-3 gap-4 sm:grid-cols-3 lg:grid-cols-3">
-          {features.map((feature, index) => (
-            <div
-              key={index}
-              className="bg-blue-100 text-center p-4 rounded-lg shadow-md flex flex-col items-center justify-center min-h-32"
-            >
-              <div className="text-xl text-blue-600 mb-2">{feature.icon}</div>
-              <p className="text-gray-800 text-xs text-center">{feature.text}</p>
-            </div>
-          ))}
-        </div>
+    <div className="max-w-6xl mx-auto px-4">
+      <h1 className="text-3xl md:text-4xl font-bold mb-6 text-center text-gray-800">
+        {t('features.title')}
+      </h1>
+      <div className="grid grid-cols-3 gap-4 sm:grid-cols-3 lg:grid-cols-3">
+        {features.map((feature, index) => (
+          <div
+            key={index}
+            className="bg-blue-100 text-center p-4 rounded-lg shadow-md flex flex-col items-center justify-center min-h-32"
+          >
+            <div className="text-xl text-blue-600 mb-2">{feature.icon}</div>
+            <p className="text-gray-800 text-xs text-center">{feature.text}</p>
+          </div>
+        ))}
       </div>
+    </div>
   );
 };
 
